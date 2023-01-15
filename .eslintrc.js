8b4473d8395d8c030d0c93fcc07e548791a407a0
Ultra-max99 <80412399+ultracodez@users.noreply.github.com>
2023-01-15 13:41:42 -0800

    Add prettier config to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,19 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
   extends: [
-    'plugin:react/recommended',
-    'standard',
-    'next/core-web-vitals'
+    "plugin:react/recommended",
+    "standard",
+    "next/core-web-vitals",
+    "prettier",
   ],
-  overrides: [
-  ],
+  overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: [
-    'react'
-  ],
-  rules: {
-  }
-}
+  plugins: ["react"],
+  rules: {},
+};
