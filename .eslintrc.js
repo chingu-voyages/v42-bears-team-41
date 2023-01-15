3aa69bbb21487197d008dbf6247824ceabb4aa8b
Eduardo A. Maroto Campos <ultracodez@outlook.com>
2023-01-15 13:30:34 -0800

    Create .eslintrc.js
    Part of the ESLint Github Action Setup

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard',
+    'next/core-web-vitals'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+  }
+}
