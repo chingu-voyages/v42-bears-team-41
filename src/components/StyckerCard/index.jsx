7c67c30d44394dbe6b3c76577971ba6f206e2a18
Ultra-max99 <80412399+ultracodez@users.noreply.github.com>
2023-01-22 08:14:31 -0800

    Some component changes, new LinkWrapper

--- src/components/StyckerCard/index.jsx
+++ src/components/StyckerCard/index.jsx
@@ -1,34 +1,38 @@
+/* eslint-disable @next/next/no-img-element */
 export function StyckerCard(props) {
   return (
     <>
-      <div className="card card-bordered card-compact w-96 bg-base-100 shadow-xl border-secondary">
+      <div className="card card-bordered card-compact w-fit h-fit pb-2 px-1 bg-base-100 shadow-xl border-secondary">
         <div className="card-body">
           <div className="flex">
-            <div className="flex auto w-18 mr-4 mt-2">
+            <div className="flex auto w-18 mr-6 mt-2">
               <div class="flex flex-col w-12">
                 <div className="avatar flex justify-center">
                   <div className="w-12 rounded">
-                    <img
-                      src="https://placeimg.com/192/192/people"
-                      alt="Tailwind-CSS-Avatar-component"
-                    />
+                    {props.user.avatar_url ? (
+                      <img
+                        src={props.user.avatar_url}
+                        alt={`${props.user.name}'s avatar`}
+                      />
+                    ) : (
+                      <div className="avatar placeholder">
+                        <div className="bg-neutral-focus text-neutral-content rounded-full w-12">
+                          <span className="text-2xl">
+                            {`${props.user.name}`.charAt(0).toUpperCase()}
+                          </span>
+                        </div>
+                      </div>
+                    )}
                   </div>
                 </div>
                 <h4 className="mt-2 text-center text-sm font-semibold">
-                  John Turner
+                  {props.user.name}
                 </h4>
               </div>
             </div>
-            <div className="flex-auto w-64">
-              <h2 class="card-title">{props.title}</h2>
-              <p>
-                This is a very long description that showcases the Stycker Card
-                component. This is a very long description that showcases the
-                Stycker Card component. This is a very long description that
-                showcases the Stycker Card component. This is a very long
-                description that showcases the Stycker Card component. This is a
-                very long description that showcases the Stycker Card component.
-              </p>
+            <div className="flex-auto w-72">
+              <h2 className="card-title">{props.title}</h2>
+              <p>{props.description}</p>
             </div>
           </div>
         </div>
--- src/pages/testing.jsx
+++ src/pages/testing.jsx
@@ -1,11 +1,18 @@
+import { LinkWrapper } from "@/components/LinkWrapper";
 import { StyckerCard } from "@/components/StyckerCard";
 
 export default function TestingPage() {
   return (
     <>
-      <div className="m-8">
-        <StyckerCard title="Stycker Card Component" />
-      </div>
+      <LinkWrapper href="https://google.com" className="m-8">
+        <StyckerCard
+          title="Stycker Card Component"
+          description={
+            "This is a short sample description. This is a short sample description. This is a short sample description. This is a short sample description. This is a short sample description. This is a short sample description. This is a short sample description. This is a short sample description. This is a short sample description. This is a short sample description. This is a short sample description."
+          }
+          user={{ name: "Bryan Moore" }}
+        />
+      </LinkWrapper>
     </>
   );
 }
