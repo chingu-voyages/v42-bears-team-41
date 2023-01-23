533540a6f6ab843614ce87a663b42be6345561c9
ultracodez <cuentajugar2018@hotmail.com>
2023-01-23 22:47:51 +0000

    Create `/expore` route and add Gitpod config file

---
+++ src/pages/explore.jsx
@@ -0,0 +1,11 @@
+import StackGrid from "react-stack-grid";
+
+export default function ExplorePage() {
+  return (
+    <>
+      <StackGrid columnWidth={150}>
+        
+      </StackGrid>
+    </>
+  );
+}
