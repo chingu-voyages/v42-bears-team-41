6b65b9774910609db676bec26f2f3dc1ebacaa8d
ultracodez <cuentajugar2018@hotmail.com>
2023-01-24 23:52:11 +0000

    `/explore` route completed

--- src/pages/explore.jsx
+++ src/pages/explore.jsx
@@ -3,8 +3,32 @@ import { DividerArea } from "@/components/DividerArea";
 import { StyckerCardWithFixedAdjustableHeight } from "@/components/StyckerCard";
 import StackGrid from "react-stack-grid";
 import { createSampleStyckerCardDataArray } from "../../.testing/createSampleStyckerCardData";
+import Select from "react-tailwindcss-select";
+import { useState } from "react";
 
+const filterValues = [
+  { value: "fox", label: "🦊 Fox" },
+  { value: "Butterfly", label: "🦋 Butterfly" },
+  { value: "Honeybee", label: "🐝 Honeybee" },
+];
+const sortByValues = [
+  { value: "created", label: "Date Created" },
+  { value: "views", label: "Views" },
+  { value: "placeholder", label: "Placeholder" },
+];
 export default function ExplorePage() {
+  const [filters, setFilters] = useState(null);
+
+  const handleFilterChange = (value) => {
+    setFilters(value);
+  };
+
+  const [sortByValue, setSortByValue] = useState(null);
+
+  const handleSortByValueChange = (value) => {
+    setSortByValue(value);
+  };
+
   return (
     <>
       <Center className="mt-20">
@@ -26,17 +50,95 @@ export default function ExplorePage() {
           diam, molestie egestas sem semper consequat. Aenean nec lorem erat.
         </p>
       </Center>
-      <DividerArea className="h-14 sticky top-0 z-10">
-        <Center>
+      <DividerArea className="h-14 mt-6 sticky top-0 z-10 border-y border-accent">
+        <Center className={"mt-2"}>
           <div className="w-10/12 relative">
-            <div className="absolute left-0">a</div>
-            <div className="absolute inset-x-1/2">b</div>
-            <div className="absolute right-0 mr-4">
-              <select className="select select-accent w-full max-w-xs ">
+            <div className="h-12 absolute left-0">
+              <Select
+                value={filters}
+                onChange={handleFilterChange}
+                options={filterValues}
+                isMultiple
+                isSearchable
+                classNames={{
+                  menuButton: ({ isDisabled }) =>
+                    `flex text-sm text-neutral-content border border-gray-300 rounded shadow-sm transition-all duration-300 focus:outline-none ${
+                      isDisabled
+                        ? "bg-gray-200"
+                        : "bg-base-100  focus:border-accent-focus focus:ring focus:ring-blue-500/20" // Removed hover:border-accent because of bug
+                    }`,
+                  menu: "absolute z-10 w-full bg-base-100 shadow-lg border border-accent rounded py-1 mt-1.5 text-sm text-neutral-content",
+                  searchBox:
+                    "w-full py-2 pl-8 text-sm text-gray-500 bg-base-100 border border-accent rounded focus:border-accent-focus focus:ring-0 focus:outline-none",
+                  listItem: ({ isSelected }) =>
+                    `block transition duration-200 px-2 py-2 cursor-pointer select-none truncate rounded ${
+                      isSelected
+                        ? `text-neutral-content bg-blue-500`
+                        : `text-neutral-content hover:bg-base-content hover:text-neutral`
+                    }`,
+                  tagItemText:
+                    "text-neutral-content truncate cursor-default select-none",
+                  tagItem: ({ isDisabled }) => {
+                    const baseClass =
+                      "bg-base-100 border rounded-sm flex space-x-1";
+                    const disabledClass = isDisabled
+                      ? "border-gray-500 px-1"
+                      : "pl-1";
+                    return `${baseClass} ${disabledClass}`;
+                  },
+                  tagItemIconContainer:
+                    "flex items-center px-1 cursor-pointer rounded-r-sm  hover:text-error",
+                }}
+              />
+            </div>
+            <div className="absolute inset-x-1/2">
+              {/*
+              Reserved for future use (positioned in center)
+              */}
+            </div>
+            <div className="absolute right-0 mr-4 h-12 min-w-[12%]">
+              <Select
+                value={sortByValue}
+                onChange={handleSortByValueChange}
+                options={sortByValues}
+                classNames={{
+                  menuButton: ({ isDisabled }) =>
+                    `flex text-sm text-neutral-content border border-gray-300 rounded shadow-sm transition-all duration-300 focus:outline-none ${
+                      isDisabled
+                        ? "bg-gray-200"
+                        : "bg-base-100  focus:border-accent-focus focus:ring focus:ring-blue-500/20" // Removed hover:border-accent because of bug
+                    }`,
+                  menu: "absolute z-10 w-full bg-base-100 shadow-lg border border-accent rounded py-1 mt-1.5 text-sm text-neutral-content",
+                  searchBox:
+                    "w-full py-2 pl-8 text-sm text-gray-500 bg-base-100 border border-accent rounded focus:border-accent-focus focus:ring-0 focus:outline-none",
+                  listItem: ({ isSelected }) =>
+                    `block transition duration-200 px-2 py-2 cursor-pointer select-none truncate rounded ${
+                      isSelected
+                        ? `text-info-content bg-info`
+                        : `text-neutral-content hover:bg-base-content hover:text-neutral`
+                    }`,
+                  tagItemText:
+                    "text-neutral-content truncate cursor-default select-none",
+                  tagItem: ({ isDisabled }) => {
+                    const baseClasse =
+                      "bg-base-100 border rounded-sm flex space-x-1";
+                    const disabledClass = isDisabled
+                      ? "border-gray-500 px-1"
+                      : "pl-1";
+                    return `${baseClasse} ${disabledClass}`;
+                  },
+                  tagItemIconContainer:
+                    "flex items-center px-1 cursor-pointer rounded-r-sm  hover:text-error",
+                }}
+              />
+              {
+                // Default DaisyUI Select, unused
+                /* <select className="select border-gray-300 hover:border-accent focus:border-accent-focus w-full max-w-xs ">
                 <option>Date created</option>
                 <option>Views</option>
                 <option>Placeholder</option>
-              </select>
+              </select> */
+              }
             </div>
           </div>
         </Center>
@@ -58,6 +160,7 @@ export default function ExplorePage() {
               }
             )}
           </StackGrid>
+          <div className="mt-10"></div>
         </div>
       </Center>
     </>
