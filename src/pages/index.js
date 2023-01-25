a99dfa12360fcd73d84fe3243de46c7cd005527d
Ultra-max99 <80412399+ultracodez@users.noreply.github.com>
2023-01-25 07:20:09 -0800

    Bigger text, sample `StyckerCard`s

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,53 @@
+import { StyckerCard } from "@/components/StyckerCard";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <>
       <div className="hero min-h-screen bg-base-200">
         <div className="hero-content pr-8">
-          <div className="max-w-md flex-col">
-            <h1 className="text-3xl font-bold">Welcome to Stycker!</h1>
-            <h2 className="font-bold">
-              Find a project and make a contribution
-            </h2>
-            <p className="py-6 pr-8">
-              We created Sycker so that you can find projects that interest you.
-              Take a look around and find a project to contribute to, or add one
-              and let others contribute.{" "}
-            </p>
-            <button className="btn btn-primary text-center">Get Started</button>
+          <div className="flex">
+            <div className="mt-2 flex-auto max-w-lg flex-col">
+              <h1 className="text-6xl font-bold">
+                Welcome to <span className="text-accent">Stycker!</span>
+              </h1>
+              <h2 className="font-bold text-xl mt-4">
+                Find a project and make a contribution
+              </h2>
+              <p className="mt-2 mb-4 mr-8">
+                We created Stycker so that you can find projects that interest
+                you. Take a look around and find a project to contribute to, or
+                add one and let others contribute.{" "}
+              </p>
+              <Link
+                href="/explore"
+                className="btn btn-sm btn-primary text-center text-sm"
+              >
+                Check It Out!
+              </Link>
+            </div>
+            <div
+              className="flex-auto w rounded-xl  border border-neutral bg-base-300"
+              style={{
+                backgroundImage:
+                  "radial-gradient(circle at 2px 2px, grey 1px, transparent 0)",
+                backgroundSize: "15px 15px",
+              }}
+            >
+              <div className="px-4 py-4 ">
+                <StyckerCard
+                  title="Sample Title"
+                  user={{ name: "wow" }}
+                  description="a"
+                />
+                <div className="mt-8" />
+                <StyckerCard
+                  title="Sample Title"
+                  user={{ name: "wow" }}
+                  description="a"
+                />
+              </div>
+            </div>
           </div>
         </div>
       </div>
