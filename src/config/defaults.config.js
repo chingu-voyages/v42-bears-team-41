8c3cda2dea7b067b1ac9e0ddd3af72828021d524
ultracodez <cuentajugar2018@hotmail.com>
2023-01-26 23:14:08 +0000

    Add more base styles

--- src/config/defaults.config.js
+++ src/config/defaults.config.js
@@ -1,4 +1,4 @@
 export const DefaultTheme = "emerald";
 export const LightTheme = "emerald";
 export const DarkTheme = "night";
-export const BaseStyle = "bg-base-100";
+export const BaseStyle = "bg-base-100 h-screen w-screen";
