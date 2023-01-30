bb9525f393e1e1d00710d2a46a4b4b57fc30786a
ultracodez <cuentajugar2018@hotmail.com>
2023-01-30 22:25:17 +0000

    Lifecycle commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,9 @@ import { ThemeProvider, useTheme } from "@/components/Theme/state";
 import { ThemeToggle } from "@/components/Theme/ThemeToggle";
 import { BaseStyle, DarkTheme, LightTheme } from "@/config/defaults.config";
 import "@/styles/globals.css";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import { useState } from "react";
 
 export default function App({ Component, pageProps }) {
   let dark = false;
@@ -9,14 +12,20 @@ export default function App({ Component, pageProps }) {
     // Client-side-only code
     dark = window.matchMedia("(prefers-color-scheme: dark)").matches;
   }
+  const [supabase] = useState(() => createBrowserSupabaseClient());
 
   return (
-    <ThemeProvider defaultTheme={dark ? DarkTheme : LightTheme}>
-      <DaisyThemeSetter>
-        <ThemeToggle className="absolute top-0 right-0 mt-4 mr-4" />
-        <Component {...pageProps} />
-      </DaisyThemeSetter>
-    </ThemeProvider>
+    <SessionContextProvider
+      supabaseClient={supabase}
+      initialSession={pageProps.initialSession}
+    >
+      <ThemeProvider defaultTheme={dark ? DarkTheme : LightTheme}>
+        <DaisyThemeSetter>
+          <ThemeToggle className="absolute top-0 right-0 mt-4 mr-4" />
+          <Component {...pageProps} />
+        </DaisyThemeSetter>
+      </ThemeProvider>
+    </SessionContextProvider>
   );
 }
 
