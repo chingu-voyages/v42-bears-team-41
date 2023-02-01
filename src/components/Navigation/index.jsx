4dbcdda6391b60947810d8afb3638365b399a8e9
Elijah Barreto <51147388+ElijahBcodes@users.noreply.github.com>
2023-02-01 11:23:14 -0800

    search bar moved to right, space between home and explore needs adjustment

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -4,25 +4,25 @@ export default function Navigation() {
   return (
     <header>
       <div className="bg-base-200 navbar">
-        <ul className="inline-grid grid-cols-3 space-x-4">
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/explore">Explore</Link>
-          </li>
-          <li>
-            <Link href="/">Signup</Link>
-          </li>
-        </ul>
-        <div className="flex items-end">
-          <div className="form-control">
-            <input
-              type="text"
-              placeholder="Search"
-              className="input input-bordered"
-            />
-          </div>
+        <div>
+          <ul className="inline-grid grid-cols-8 space-x-4">
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/explore">Explore</Link>
+            </li>
+            <li>
+              <Link href="/">Signup</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="form-control">
+          <input
+            type="text"
+            placeholder="Search"
+            className="input input-bordered"
+          />
         </div>
       </div>
     </header>
