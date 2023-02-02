8583f1ce899a437c1cf44567fdb811be71bc4d8b
Elijah Barreto <51147388+ElijahBcodes@users.noreply.github.com>
2023-02-02 13:20:35 -0800

    added button spacing fixed

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -6,15 +6,21 @@ export default function Navigation() {
       <div className="bg-base-200 navbar">
         <div>
           <ul className="inline-grid grid-cols-8 space-x-4">
+          <button className="btn btn-ghost">
             <li>
               <Link href="/">Home</Link>
             </li>
+            </button>
+            <button className="btn btn-ghost">
             <li>
-              <Link href="/explore">Explore</Link>
+              <Link href="/">Log in </Link>
             </li>
+            </button>
+            <button className="btn btn-ghost">
             <li>
-              <Link href="/">Signup</Link>
+              <Link href="/">Sign up</Link>
             </li>
+            </button>
           </ul>
         </div>
         <div className="form-control">
