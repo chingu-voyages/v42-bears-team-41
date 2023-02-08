c3f1894289e859f4c07dee350032ffc606c295d7
Eduardo A. Maroto Campos <ultracodez@outlook.com>
2023-02-08 04:45:42 +0000

    lifecycle commit

--- src/pages/stycker/[id].jsx
+++ src/pages/stycker/[id].jsx
@@ -35,10 +35,11 @@ export default function ExpandedPage() {
         console.log(err);
       }
     }
-    preloadData();
+    if (!(id.length === 12)) setData(null);
+    else preloadData();
   }, [id]);
 
-  return (
+  return data ? (
     <div className="bg-base-100">
       <div className="flex mx-20">
         <div className="grid h-20 flex-grow card rounded-box ">
@@ -191,6 +192,25 @@ export default function ExpandedPage() {
             </div>
           </div>
         </div>
+      </div>
+    </div>
+  ) : (
+    <div class="grid h-screen px-4 bg-white place-content-center">
+      <div class="text-center">
+        <h1 class="font-black text-gray-200 text-9xl">404</h1>
+
+        <p class="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          Uh-oh!
+        </p>
+
+        <p class="mt-4 text-gray-500">We can't find that page.</p>
+
+        <a
+          href="#"
+          class="inline-block px-5 py-3 mt-6 text-sm font-medium text-white bg-indigo-600 rounded hover:bg-indigo-700 focus:outline-none focus:ring"
+        >
+          Go Back Home
+        </a>
       </div>
     </div>
   );
