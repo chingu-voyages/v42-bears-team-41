0c28c0471d1735187e91a31ff87641aee9721081
gracesuhyun <gracesuhyun@gmail.com>
2023-02-13 22:05:16 -0500

    profile page backend

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,5 +1,6 @@
-import { easyLoadUser } from "@/backend/auth/easyGetUser";
-import { updateProfile } from "@/backend/auth/updateProfile";
+/* eslint-disable camelcase */
+// import { easyLoadUser } from "@/backend/auth/easyGetUser";
+// import { updateProfile } from "@/backend/auth/updateProfile";
 import { useSupabaseClient, useUser } from "@supabase/auth-helpers-react";
 import { useEffect, useState } from "react";
 
@@ -7,116 +8,198 @@ import { DividerArea } from "@/components/DividerArea";
 import Center from "@/components/Center";
 import Image from "next/image";
 
-export default function ProfileExamplePage() {
-
-  // EXAMPLE PROFILE
-  // const user = {
-  //   id: "e298331f-69c6-441f-8371-d4fb25fb6494",
-  //   username: "ultra",
-  //   avatar_url: "https://picsum.photos/300/400",
-  //   website: "https://example.org",
-  //   full_name: "Rando Person",
-  //   updated_at: null,
-  //   about_me: "I am a Rando Person",
-  //   favorite_list: ["BSON"],
-  // };
-
-  const [user, setUser] = useState({});
-
-  const supabase = useSupabaseClient();
-  const supabaseUser = useUser();
+export default function Account({ session }) {
+  const supabase = useSupabaseClient()
+  const user = useUser()
+  const [avatar_url] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [about_me, setAboutMe] = useState(null)
+  const [full_name, setFullName] = useState(null)
+  const [username, setUsername] = useState(null)
+  const [website, setWebsite] = useState(null)
+  const [favorite_list, setFavoriteList] = useState(true)
 
   useEffect(() => {
-    if (supabaseUser) easyLoadUser(supabase, supabaseUser, setUser);
-  }, [supabaseUser]);
+    getProfile()
+  }, [session])
+
+async function getProfile() {
+  try {
+    setLoading(true)
+
+    const { data, error, status } = await supabase
+      .from('profiles')
+      .select(`about_me, full_name, username, website, favorite_list, avatar_url`)
+      .eq('id', user.id)
+      .single()
+
+    if (error && status !== 406) {
+      throw error
+    }
+
+    if (data) {
+      setAboutMe(data.about_me)
+      setFullName(data.full_name)
+      setUsername(data.username)
+      setWebsite(data.website)
+      setFavoriteList(data.favorite_list)
+    }
+  } catch (error) {
+    alert('Error loading user data!')
+    console.log(error)
+  } finally {
+    setLoading(false)
+  }
+}
+
+async function updateProfile({ about_me, full_name, username, website, favorite_list }) {
+  try {
+    setLoading(true)
+
+    const updates = {
+      id: user.id,
+      full_name,
+      username,
+      website,
+      about_me,
+      favorite_list,
+      updated_at: new Date().toISOString(),
+    }
+
+    const { error } = await supabase.from('profiles').upsert(updates)
+    if (error) throw error
+    alert('Profile updated!')
+  } catch (error) {
+    alert('Error updating the data!')
+    console.log(error)
+  } finally {
+    setLoading(false)
+  }
+}
+
+// export default function ProfileExamplePage() {
+//   const [user, setUser] = useState({});
+
+//   // TEST PROFILE
+//   // const user = {
+//   //   id: "e298331f-69c6-441f-8371-d4fb25fb6494",
+//   //   username: "ultra",
+//   //   avatar_url: "https://picsum.photos/500/500",
+//   //   website: "https://example.org",
+//   //   full_name: "Rando Person",
+//   //   updated_at: null,
+//   //   about_me: "I am a Rando Person",
+//   //   favorite_list: ["BSON"],
+//   // };
+
+//   const supabase = useSupabaseClient();
+//   const supabaseUser = useUser();
+
+//   useEffect(() => {
+//     if (supabaseUser) easyLoadUser(supabase, supabaseUser, setUser);
+//   }, [supabase, supabaseUser]);
 
   return (
     <div>      
-      <div className="flex justify-center my-10">
+      <div className="flex flex-col sm:flex-row justify-center my-10">
 
         <div className="m-10">
-          <div className="avatar">
-            <div className="rounded">
-              <Image src={user.avatar_url} alt="profile picture" width={400} height={200}/>
+          <div className="avatar block">
+            <div className="rounded max-w-s max-h-52 mx-auto">
+              <Image 
+                src={avatar_url} 
+                alt="profile picture" 
+                width={400} height={200}/>
             </div>
           </div>
 
           <div>
-            <label className="mt-5 block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-first-name">
+            <label className="mt-5 block uppercase tracking-wide text-xs font-bold mb-2" htmlFor="grid-first-name">
               About Me
             </label>
             <textarea 
-              className="textarea-md appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
+              className="textarea-md appearance-none block w-full bg-gray-200 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white" 
               placeholder="About Me"
-              value={user.about_me}/>
+              value={about_me || ''}
+              onChange={(e) => setAboutMe(e.target.value)}/>
           </div>
         </div>
 
-        <form className="grow w-full max-w-lg mx-10">
+        <form className="grow max-w-lg mx-10">
           <div className="flex flex-wrap -mx-3 mb-6">
 
           <div className="w-full p-2">
-              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-name">
+              <label className="block uppercase tracking-wide text-xs font-bold mb-2" htmlFor="grid-name">
                 Full Name
               </label>
               <input 
-                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
+                className="appearance-none block w-full bg-gray-200 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
                 id="grid-name" 
-                type="name" 
+                type="text" 
                 placeholder="Full Name"
-                value={user.full_name}/>
+                value={full_name || ''}
+                onChange={(e) => setFullName(e.target.value)}/>
             </div>
 
             <div className="w-full p-2">
-              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-username">
+              <label className="block uppercase tracking-wide text-xs font-bold mb-2" htmlFor="grid-username">
                 Username
               </label>
               <input 
-                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
+                className="appearance-none block w-full bg-gray-200 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
                 id="grid-username" 
-                type="username" 
+                type="text" 
                 placeholder="Username"
-                value={user.username}/>
+                value={username || ''}
+                onChange={(e) => setUsername(e.target.value)}/>
             </div>
 
             <div className="w-full p-2">
-              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-password">
+              <label className="block uppercase tracking-wide text-xs font-bold mb-2" htmlFor="grid-password">
                 Password
               </label>
               <input 
-                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
+                className="appearance-none block w-full bg-gray-200 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
                 id="grid-password" 
-                type="password" 
+                type="text" 
                 placeholder="******************"/>
             </div>
 
             <div className="w-full p-2">
-              <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-website">
+              <label className="block uppercase tracking-wide text-xs font-bold mb-2" htmlFor="grid-website">
                 Website
               </label>
               <input 
-                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
+                className="appearance-none block w-full bg-gray-200 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" 
                 id="grid-website" 
-                type="website" 
+                type="text" 
                 placeholder="Personal Website"
-                value={user.website}/>
+                value={website || ''}
+                onChange={(e) => setWebsite(e.target.value)}/>
             </div>
           </div>
 
           <button
           className="btn"
-          onClick={() => {
-            
-            updateProfile(user, supabase);
-          }}
+          // onClick={() => {
+          //   updateProfile(user, supabase);
+          // }}
+          onClick={() => updateProfile({ about_me, full_name, username, website, favorite_list })}
+          disabled={loading}
           >
+            {loading ? 'Loading ...' : 'Update'}
             Edit Details
           </button>
 
-          <button
+          {/* <button
           className="btn btn-error float-right">
             Delete Account
+          </button> */}
+
+          <button className="button block" onClick={() => supabase.auth.signOut()}>
+            Sign Out
           </button>
+
         </form>
       </div>
 
@@ -124,7 +207,7 @@ export default function ProfileExamplePage() {
         <Center className={"mt-2"}>
           <div className="w-10/12 relative">
             <div className="h-12 absolute left-0">
-            <label className="my-3 block uppercase tracking-wide text-gray-700 text-xs font-bold" htmlFor="grid-first-name">
+            <label className="my-3 block uppercase tracking-wide text-xs font-bold" htmlFor="grid-first-name">
               My Projects
             </label>
             </div>
