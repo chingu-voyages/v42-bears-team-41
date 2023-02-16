ba56885dee1edfdd397f3a207c4ee03cd0ea22fc
Eduardo A. Maroto Campos <ultracodez@outlook.com>
2023-02-16 19:05:41 +0000

    Fix responsiveness

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,10 +24,10 @@ export default function Home() {
 
   return (
     <>
-      <div className="hero min-h-screen bg-base-100 -mt-16">
+      <div className="hero min-h-screen bg-base-100 -mt-12">
         <div className="hero-content pr-8">
-          <div className="md:flex">
-            <div className="mt-16 flex-auto max-w-lg flex-col">
+          <div className="lg:flex">
+            <div className="mt-16 text-center lg:text-start flex-auto max-w-lg flex-col pb-8">
               <h1 className="text-6xl font-bold">
                 Welcome to <span className="text-accent">Stycker!</span>
               </h1>
@@ -47,7 +47,7 @@ export default function Home() {
               </Link>
             </div>
             <div
-              className={`flex-auto w rounded-xl border bg-base-100 sm:max-h-screen lg:max-h-full`}
+              className={`flex-auto w-fit rounded-xl border bg-base-100 sm:max-h-screen lg:max-h-full`}
               style={{
                 borderColor:
                   mode === "dark" ? "hsl(var(--nf))" : "hsl(var(--b3))",
