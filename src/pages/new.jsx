ee975a08babfe76bfca5998b9daebe47845127ad
Eduardo A. Maroto Campos <ultracodez@outlook.com>
2023-02-17 22:14:24 +0000

    lifecycle commit

--- src/pages/new.jsx
+++ src/pages/new.jsx
@@ -3,10 +3,15 @@ import Center from "@/components/Center";
 import { TextareaAutosize } from "@mui/base";
 import { useSupabaseClient, useUser } from "@supabase/auth-helpers-react";
 import { useEffect, useState } from "react";
-
+import { Dropzone, DropzoneProps, IMAGE_MIME_TYPE } from "@mantine/dropzone";
 import { useForm } from "react-hook-form";
+import { useTheme } from "@/components/Theme/state";
+import Image from "next/image";
+import { AspectRatio } from "react-aspect-ratio";
 
 export default function NewStycker() {
+  const { mode } = useTheme();
+
   const {
     register,
     handleSubmit,
@@ -18,6 +23,9 @@ export default function NewStycker() {
 
   const [user, setUser] = useState({});
   const [rawUser, setRawUser] = useState({});
+
+  const [imageURL, setImageURL] = useState(null);
+  const [rawImageURL, setRawImageURL] = useState(null);
 
   const supabase = useSupabaseClient();
   const supabaseUser = useUser();
@@ -47,10 +55,86 @@ export default function NewStycker() {
       </Center>
       <Center className="w-full mt-10">
         <form
-          className="grow w-full max-w-lg"
+          className="grow w-full max-w-lg "
           onSubmit={handleSubmit(onSubmit)}
         >
-          <div className="w-full p-2">
+          <div className="border-neutral-focus" style={{ display: "none" }}>
+            {" "}
+            These invisible elements allow the dynamic classes to compile
+          </div>
+          <div className="border-base-300" style={{ display: "none" }}>
+            {" "}
+            These invisible elements allow the dynamic classes to compile{" "}
+          </div>
+          <Dropzone
+            className={`rounded-xl bg-base-100 hover:bg-base-200 border-[1.5px] border-dashed border-${
+              mode === "dark" ? "neutral-focus" : "base-300"
+            }`}
+            accept={IMAGE_MIME_TYPE}
+            maxSize={3 * 1024 ** 2}
+            multiple={false}
+            onDrop={async (files) => {
+              if (imageURL) {
+                await supabase.storage.from("styckers").remove(rawImageURL);
+              }
+
+              const url = `${
+                user.id
+              }/${Math.random()}/${Date.now().toString()}`;
+              const bannerImage = files[0];
+              const { data, error } = await supabase.storage
+                .from("styckers")
+                .upload(url, bannerImage, {
+                  cacheControl: "3600",
+                  upsert: false,
+                });
+              if (error) {
+                alert("Image failed to upload; Check console for more info");
+                alert(JSON.stringify(error + " " + error.message + " " + url));
+                console.error(error);
+              } else if (data) {
+                alert("Image uploaded successfully");
+                setRawImageURL(data.path);
+                setImageURL(
+                  (
+                    await supabase.storage
+                      .from("styckers")
+                      .getPublicUrl(data.path)
+                  ).data.publicUrl
+                );
+              }
+            }}
+          >
+            {!imageURL ? (
+              <div className=" mx-2 py-16">
+                <Center className={`text-2xl text-base-300`}>
+                  Upload Banner
+                </Center>
+
+                <Center className={`text-md text-base-300`}>
+                  Maximum size 5MB
+                </Center>
+              </div>
+            ) : (
+              <>
+                <AspectRatio
+                  ratio="16/9"
+                  style={{
+                    minWidth: "12rem",
+                    maxWidth: "26rem" /* equivalent of tailwindCSS w-72 */,
+                  }}
+                >
+                  <Image
+                    fill
+                    style={{ objectFit: "contain" }}
+                    src={imageURL}
+                    alt="Banner image for your stycker"
+                  />
+                </AspectRatio>
+              </>
+            )}
+          </Dropzone>
+          <div className="w-full p-2 mt-4">
             <label
               className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
               htmlFor="grid-username"
